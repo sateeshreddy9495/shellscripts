0a5a147420a766e10e98dbc7bdb491065ef35daf
root <root@client.(none)>
2016-04-19 01:08:56 -0500

    This is my first project commit welcome

---
+++ read.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo -e "Hello team, please type a word: \c "
+read word
+echo "you typed: $word"
+echo -e "hello again, Please type two words "
+read word1 word2
+echo "you typed: \"$word1\" \"$word2\""

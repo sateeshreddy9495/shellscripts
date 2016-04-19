9e6ab13fbdf0feb9eac6c27be57dbc7aed02bf91
awsuser <udevops@gmail.com>
2016-04-19 04:24:45 -0500

    added to project scripting

---
+++ find.py
@@ -0,0 +1,6 @@
+import sys
+import os
+if os.path.exists("/tmp/abcdefg"):
+    print("hey i found this file")
+else:
+    print("no file found")
---
+++ python.sh
@@ -0,0 +1,6 @@
+import sys
+import os
+message = "hello world"
+print(message)
+print(sys.argv[0])
+print("hello " + sys.argv[1])
